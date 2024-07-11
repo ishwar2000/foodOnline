66a45cb8c1a33fedb9c06d2d7079447f289e3fe2
ishwar patgar <ishwarpatgar2000@gmail.com>
2024-07-12 00:11:54 +0530

    fishy

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -156,5 +156,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vishupatgar12345@gmail.com'
-EMAIL_HOST_PASSWORD = 'igarqyfnvotpyhxi'
